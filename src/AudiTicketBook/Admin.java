c449e79e77a82106430527a0d29d8dca3cfc6848
DhruvShrimali <95020813+DhruvShrimali@users.noreply.github.com>
2022-11-30 22:01:03 +0530

    Update Admin.java

--- src/AudiTicketBook/Admin.java
+++ src/AudiTicketBook/Admin.java
@@ -1,12 +1,15 @@
 package AudiTicketBook;
 
-public class Admin extends User{
+public class Admin extends User implements Runnable{
+    Thread t;
 
     public Admin(String s,String p,String n)
     {
         super(s,p,n);
+        t = new Thread(this);
+        t.start();
     }
-
+    public void run(){}
     public void addEvent(Event e)
     {
         Auditorium.addEvent(this,e);
